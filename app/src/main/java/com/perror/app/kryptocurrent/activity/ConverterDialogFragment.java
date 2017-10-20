ebfd7314bdab310c80408b30a77834431e04f7bf
ndusunday <ndusunday@gmail.com>
2017-10-20 17:06:43 +0100

    README included

---
+++ app/src/main/java/com/perror/app/kryptocurrent/activity/ConverterDialogFragment.java
@@ -0,0 +1,10 @@
+package com.perror.app.kryptocurrent.activity;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by ndu on 10/20/17.
+ */
+
+public class ConveterDialogFragment extends DialogFragment {
+}
